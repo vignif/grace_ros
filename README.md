e7cb876f35b722348446a9f9fa442dc7bd41355c
Francesco Vigni <vignif@gmail.com>
2022-09-17 15:00:05 +0200

    fix server with oop

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # grace_ros
 ROS wrapper for GRACE - GeometRic ApproaCh to mutual Engagement
+
+Create your ros workspace
+```
+cd ~
+mkdir -p ros_ws/src
+```
+
+Get the current ros package
+```
+cd ~/ros_ws/src
+git clone git@github.com:vignif/grace_ros.git
+```
+
+Explicit external dependecy of GRACE
+git@github.com:vignif/grace.git
+
+
+```
+cd ~/ros_ws
+python -m venv venv_grace
+source venv_grace/bin/activate
+```
+
+```
+pip install --upgrade pip
+wget https://github.com/vignif/grace/releases/download/v1.0.0/grace-1.0.0-py3-none-any.whl
+pip install grace-1.0.0-py3-none-any.whl
+```
+
+Build your package
+
+```
+catkin build
+```
+
+Source the bin of the ros package
+
+```
+source ./devel/setup.sh
+```
+
+You should be ready to go now.
+
+Try to run the demo communication with:
+
+```
+roslaunch grace_ros demo.launch
+```
+
+At this point grace is running and you should have:
+
+- An rviz visualization pops up showing a human reference frame moving with respect to the robot.
+
+- The real-time plot of the computed mutual engagement (published on the topic `/mutual_engagement`) 
--- src/server.py
+++ src/server.py
@@ -1,40 +1,77 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+from grace.grace import Interaction, Agent, FeatureHandler, ProximityFeature, GazeFeature, run
 
-from engagement_system.srv import GetEngagement
+
+from grace_ros.srv import GetEngagement
 import rospy
-
+from std_msgs.msg import Float32
 from dynamic_reconfigure.server import Server
-from engagement_system.cfg import EngConfig
+from grace_ros.cfg import EngConfig
 
-def callback(config, level):
-    rospy.loginfo("""Reconfigure Request: {proxemics_weight_param}, {gaze_weight_param}""".format(**config))
-    return config
+import logging
 
-def handle_engagement(req):
-    AgentA = req.A
-    AgentB = req.B
-    engagement = 0
-    
-    prox = rospy.get_param("proxemics_weight")
-    gaze = rospy.get_param("gaze_weight")
+eng_logger = logging.getLogger("grace.grace")
+eng_logger.setLevel(logging.WARNING)
 
-    ### CALL ENGAGEMENT LIBRARY FROM HERE ###
-    
-    rospy.loginfo(f'Social Agent name A: {AgentA.name}')
-    rospy.loginfo(f'Social Agent name B: {AgentB.name}')
 
-    rospy.loginfo("Engagement request received")
-    
-    return engagement
+class GraceServer:
+    def __init__(self):
+        self.service = rospy.Service(
+            "get_engagement", GetEngagement, self.handle_engagement)
+        self.srv = Server(EngConfig, self.callback)
+        self.pub = rospy.Publisher('engagement', Float32, queue_size=10)
+        self.eng = 0.0
 
-def engagement_server():
-    rospy.init_node('system_hri')
-    s = rospy.Service('get_engagement', GetEngagement, handle_engagement)
-    rospy.loginfo("Ready to receive agents of an interaction")
-    srv = Server(EngConfig, callback)
-    rospy.spin()
+    def callback(self, config, level):
+        proxemics_weight_param = config["proxemics_weight"]
+        gaze_weight_param = config["gaze_weight"]
+        rospy.loginfo(
+            f"Reconfigure Request: {proxemics_weight_param}, {gaze_weight_param}")
+        return config
+
+    def handle_engagement(self, req):
+
+        positionA = [req.A.Pose.position.x,
+                     req.A.Pose.position.y, req.A.Pose.position.z]
+        orientationA = [req.A.Pose.orientation.x, req.A.Pose.orientation.y,
+                        req.A.Pose.orientation.z, req.A.Pose.orientation.w]
+
+        positionB = [req.B.Pose.position.x,
+                     req.B.Pose.position.y, req.B.Pose.position.z]
+        orientationB = [req.B.Pose.orientation.x, req.B.Pose.orientation.y,
+                        req.B.Pose.orientation.z, req.B.Pose.orientation.w]
+
+        AgentA = Agent(req.A.name, positionA, orientationA)
+        AgentB = Agent(req.B.name, positionB, orientationB)
+        # prox = rospy.get_param("proxemics_weight")
+        # gaze = rospy.get_param("gaze_weight")
+
+        ### CALL ENGAGEMENT LIBRARY FROM HERE ###
+
+        rospy.loginfo_once(f'Social Agent name A: {AgentA.name}')
+        rospy.loginfo_once(f'Social Agent name B: {AgentB.name}')
+
+        P = ProximityFeature(AgentA, AgentB)
+        P.epsilon = 1.5
+        G = GazeFeature(AgentA, AgentB)
+
+        # feature handler
+        F = FeatureHandler(AgentA, AgentB)
+        F.add(P, 1.0)
+        F.add(G, 1.0)
+        F.compute()
+        I = Interaction(F)
+        self.eng = I.compute()
+        rospy.loginfo(f'Mutual Engagement: {self.eng}')
+
+        return self.eng
+
 
 if __name__ == "__main__":
-    engagement_server()
+    rospy.init_node('system_hri')
+    rospy.loginfo("Ready to compute mutual engagement")
+    GraceServer()
+
+    rospy.spin()
